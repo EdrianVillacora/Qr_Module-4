124f22825171915edb164e1ff7127733f3dc242c
EdrianVillacora <Edrianvillacors1@gmail.com>
2026-08-14 17:26:19 +0800

    Initial commit

---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: '#1565C0',
+  background: '#ce0ed4',
+  card: '#13e9e9',
+  textPrimary: '#51e20e',
+  textSecondary: '#546E7A',
+  textOnPrimary: '#0ce290',
+  surface: '#E3F2FD',
+  border: '#E0E8F0',
+  shadow: '#7005b8',
+} as const;
